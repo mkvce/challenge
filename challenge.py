11f47bf6e6734d6920b45bc0e958101d03f1d2fc
Mahdi <mahdi57.kamali1@gmail.com>
2021-07-08 10:59:27 +0430

    Refactor code

--- challenge.py
+++ challenge.py
@@ -1,1 +1,13 @@
-print(sum((x for x in range(1000) if x % 3 == 0 or x % 5 == 0)))
+def calculate_sum_of_multiples(max_num: int, *args):
+    s = 0
+    for x in range(max_num):
+        reminders = [x % arg for arg in args]
+        if not all(reminders):
+            s += x
+    return s
+
+def main():
+    print(calculate_sum_of_multiples(1000, 3, 5))
+
+if __name__ == '__main__':
+    main()
