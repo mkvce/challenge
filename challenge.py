374f12d1c2548544d1800302083c2b7e3c6dede4
Mahdi <mahdi57.kamali1@gmail.com>
2021-07-08 11:30:09 +0430

    Solve problem in constant time

--- challenge.py
+++ challenge.py
@@ -1,12 +1,30 @@
-def calculate_sum_of_multiples(max_num: int, *multiples) -> int:
+def calculate_sum_of_multiples_old(max_num: int, *multiples)->int:
     s = 0
     for x in range(max_num):
         if not all((x % multiple for multiple in multiples)):
             s += x
     return s
 
+def get_sum_of_multiples(max_num: int, mult: int)->int:
+    if mult > max_num:
+        return 0
+    first_multiple = mult
+    last_multiple = max_num - (max_num%mult)
+    num_of_multiples = (last_multiple-first_multiple) // mult + 1
+    return (first_multiple+last_multiple) * num_of_multiples // 2
+    
+
+def calculate_sum_of_multiples(max_num: int, mult1: int, mult2: int)->int:
+    sum_of_mult1_multiples = get_sum_of_multiples(max_num, mult1)
+    sum_of_mult2_multiples = get_sum_of_multiples(max_num, mult2)
+    sum_of_common_multiples = get_sum_of_multiples(max_num, mult1 * mult2)
+    return sum_of_mult1_multiples + sum_of_mult2_multiples - sum_of_common_multiples
+
 def main():
-    print(calculate_sum_of_multiples(1000, 3, 5))
+    max_num = 999
+    mult1 = 3
+    mult2 = 5
+    print(calculate_sum_of_multiples(max_num, mult1, mult2))
 
 if __name__ == '__main__':
     main()
